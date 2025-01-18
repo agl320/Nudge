3bcbd14261898d4835243c13984ff2b846908de8
Jerr-z <zmx201518@gmail.com>
2025-01-18 12:55:36 -0800

    init meeting page

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,10 +5,12 @@ import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import NotFound from "./components/ui/NotFound/NotFound";
 import App from "./App";
+import Meeting from "./meeting-page/Meeting";
 
 const router = createBrowserRouter([
     { errorElement: <NotFound /> },
     { path: "/", element: <App /> },
+    { path: "/meeting/:id", element: <Meeting /> },
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
---
+++ frontend/src/meeting-page/Meeting.tsx
@@ -0,0 +1,9 @@
+
+
+export default function Meeting() {
+    return (
+        <div>
+            <h1>Meeting</h1>
+        </div>
+    );
+}
