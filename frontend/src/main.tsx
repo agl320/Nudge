5ccb4a03aebc4efcdcdc3e5d716a0cddffbbfa41
Andrew Lai <the.andrew.lai@gmail.com>
2025-01-18 12:23:43 -0800

    updated some stuf

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,28 +1,16 @@
-import { Routes, Route } from "react-router-dom";
+import ReactDOM from "react-dom/client";
+
+import "./index.css";
+
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import NotFound from "./components/ui/NotFound/NotFound";
+import App from "./App";
 
-function Home() {
-    return <h1>Welcome to the Home Page</h1>;
-}
+const router = createBrowserRouter([
+    { errorElement: <NotFound /> },
+    { path: "/", element: <App /> },
+]);
 
-function About() {
-    return <h1>About Us</h1>;
-}
-
-function Contact() {
-    return <h1>Contact Us</h1>;
-}
-
-export default function App() {
-    return (
-        <div>
-            <h1>My React Router App</h1>
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/about" element={<About />} />
-                <Route path="/contact" element={<Contact />} />
-                <Route path="*" element={<NotFound />} />
-            </Routes>
-        </div>
-    );
-}
+ReactDOM.createRoot(document.getElementById("root")!).render(
+    <RouterProvider router={router} />
+);
