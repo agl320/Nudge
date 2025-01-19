9947e1ebbc7ce5fd1a0739a7d6877aa45ba058fd
Jerr-z <zmx201518@gmail.com>
2025-01-19 02:24:44 -0800

    done!!!

--- backend/main.py
+++ backend/main.py
@@ -4,8 +4,12 @@ from flask_socketio import SocketIO, emit, join_room, leave_room
 from transformer import ContextualOutlierDetector
 from transcriber import Transcriber
 from transformer import sentence_queue
-
+from llm import LLM
+from firebase import firebaseClientInstance
+from firebase import FirebaseClient
 import threading
+import time
+
 
 # Configure logging
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
@@ -17,8 +21,42 @@ socketio.init_app(app)
 
 meetings = {}  # Dictionary to keep track of meetings and participants
 context_detectors = {}  # Map<meeting_id, ContextOutlierDetector>
-transcriber = Transcriber()  # 1 instance is enough
 
+transcriber = Transcriber()  # 1 instance is enough
+llm = LLM()
+
+
+@app.route('/api/create_meeting', methods=['POST'])
+def create_meeting():
+    # create Meeting document in Firestore
+    data = request.json
+    print(data)
+    meeting_id = data.get('meeting_id')
+    current_activity = data.get('current_activity')
+    start_time = data.get('start_time')
+    role = data.get('role')
+    setting = data.get('setting')
+    activities = data.get('activities')
+    
+    firebaseClientInstance.create_meeting(meeting_id, current_activity, start_time, role, setting, activities)
+
+    # Use LLM to create the initial list of sentences to populate meeting context
+    llm.queue.put((meeting_id, role, setting, activities))
+    
+    return jsonify({'message': 'Meeting created successfully'}), 200
+    
+
+@app.route('/api/switch_activity', methods=['POST'])
+def initialize_context():
+    data = request.json
+    meeting_id = data.get('meeting_id')
+    next_activity = data.get('next_acitivity')
+    current_epoch_time = int(time.time())
+    firebaseClientInstance.update_meeting(meeting_id, current_activity=next_activity, start_time=current_epoch_time)
+
+    return jsonify({'message': 'Activity switched successfully'}), 200
+
+    
 
 @socketio.on('join_meeting')
 def join_meeting(data):
@@ -50,6 +88,10 @@ def leave_meeting(data):
 
     if meeting_id in meetings and not meetings[meeting_id]:
         del meetings[meeting_id]
+        try:
+            FirebaseClient.delete_meeting(meeting_id)
+        except Exception as e:
+            logger.error(f"Error removing meeting {meeting_id} from Firestore: {str(e)}")
 
 
 @socketio.on('connect')
@@ -105,6 +147,7 @@ def context_detection_worker():
     logger.info("Starting context detection worker")
     while True:
         timestamp, meeting_id, user_id, sentence = sentence_queue.get()
+        emit("transcription", {'user_id': user_id, 'sentence': sentence}, room=meeting_id)
         detector = context_detectors.get(meeting_id)
         if not detector:
             context_detectors[meeting_id] = ContextualOutlierDetector()
@@ -114,9 +157,17 @@ def context_detection_worker():
             logger.warning(f"Outlier detected for user {user_id} in meeting {meeting_id}: {sentence}")
             socketio.emit('outlier_detected', {'user_id': user_id, 'sentence': sentence}, room=meeting_id)
 
+def llm_worker():
+    logger.info("Starting LLM worker")
+    while True:
+        meeting_id, role, setting, activities = llm.queue.get()
+        print(f"Fetched from queue, creating context for meeting {meeting_id}")
+        llm.create_context(meeting_id, role, setting, activities)
+
 
 if __name__ == '__main__':
     logger.info("Starting server...")
     threading.Thread(target=context_detection_worker, daemon=True).start()
     threading.Thread(target=transcriber.transcribe_worker, daemon=True).start()
+    threading.Thread(target=llm_worker, daemon=True).start()
     socketio.run(app, host='0.0.0.0', port=5555, debug=True, allow_unsafe_werkzeug=True)
